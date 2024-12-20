cfd86f9db4a4428d12baf7f8abb51b1fe526a668
Yuzhou Liu <yuzhou.liu@mongodb.com>
2024-12-20 16:26:09 -0500

    refactor and cleanup

--- vie_summarizer/main.py
+++ vie_summarizer/main.py
@@ -2,26 +2,26 @@ import os
 from requests import sessions
 from pprint import pprint
 from rocketchat_API.rocketchat import RocketChat
-from . import groupslist
-from . import group
+import vie_summarizer.rocketchat.rocketchat as rocketchat
 
 def main():
-    pat_userid=os.environ.get('ROCKETCHAT_PAT_USERID')
-    pat_token=os.environ.get('ROCKETCHAT_PAT_TOKEN')
+    pat_userid, pat_token = get_creds()
 
     with sessions.Session() as session:
         rocket = RocketChat(user_id=pat_userid, auth_token=pat_token, server_url='https://rc.seekingalpha.com', session=session)
-        groups_list = groupslist.GroupsList(rocket)
+        rocketChatHelper = rocketchat.RocketChatHelper(rocket)
 
         # rooms = ["VIE: Off-Topic & Trading", "Value Investor's Edge"]
         rooms_to_summarize = ["Value Investor's Edge"]
         for room_name in rooms_to_summarize:
-            room_id = groups_list.get_room_id(room_name)
+            room_id = rocketChatHelper.get_room_id(room_name)
             if room_id is None:
                 raise Exception("Room not found: " + room_name)
-            
-            room = group.Group(rocket, room_name, room_id)
-            threads = room.get_threads()
+
+            threads = rocketChatHelper.chat_list_threads(room_id, count=2).json()['threads']
 
             print("Number of threads: " + str(len(threads)))
             pprint(threads)
+
+def get_creds():
+    return os.environ.get('ROCKETCHAT_PAT_USERID'), os.environ.get('ROCKETCHAT_PAT_TOKEN')
--- vie_summarizer/rocketchat/rocketchat.py
+++ vie_summarizer/rocketchat/rocketchat.py
@@ -1,9 +1,17 @@
 from rocketchat_API.rocketchat import RocketChat
-from pprint import pprint
 
-class GroupsList:
+class RocketChatHelper:
     def __init__(self, rocket: RocketChat):
+        self.rocket = rocket
         self.groups_list = rocket.groups_list().json()
+
+    def chat_list_threads(self, room_id, **kwargs):
+        """List threads."""
+        return self.rocket.call_api_get(
+            "chat.getThreadsList",
+            rid=room_id,
+            kwargs=kwargs,
+        )
 
     def get_room_id(self, group_name):
         for group in self.groups_list['groups']:
